414e2750ec7875ab9cdf127241a74ef36a843928
martin-garcia-blanco <programadopormartin@gmail.com>
2020-06-04 18:24:49 +0200

    first commit of the workshop, installation

--- other/configuration/calculator/.babelrc.js
+++ other/configuration/calculator/.babelrc.js
@@ -1,11 +1,37 @@
+/* const isTest = String(process.env.NODE_ENV) === 'test'
+
 module.exports = {
-  presets: [['env', {modules: false}], 'react'],
+  presets: [['env', {modules: isTest ? 'commonjs' : false}], 'react'],
   plugins: [
     'syntax-dynamic-import',
     'transform-class-properties',
     'transform-object-rest-spread',
   ],
+} */
+
+const isTest = String(process.env.NODE_ENV) === 'test'
+
+// Babel needs to recieve it's config as a function on module.exports so we
+// have to wrap our object in a function
+const test = (api) => {
+  // Babel requires api.cache(true | false) to be set
+  api.cache(true)
+
+  return {
+    presets: [
+      ['@babel/preset-env', {modules: isTest ? 'commonjs' : false}],
+      '@babel/preset-react',
+    ],
+    plugins: [
+      'syntax-dynamic-import',
+      'transform-class-properties',
+      '@babel/plugin-proposal-object-rest-spread',
+      isTest ? 'dynamic-import-node' : null,
+    ],
+  }
 }
+
+module.exports = test
 
 /*
 Solution snippets below
---
+++ other/configuration/calculator/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  moduleNameMapper: {
+    '\\.module\\.css$': 'identity-obj-proxy',
+    '\\.css$': require.resolve('./test/style-mock'),
+  },
+  setupTestFrameworkScriptFile: require.resolve(
+    './test/setup-test-framework.js',
+  ),
+}
